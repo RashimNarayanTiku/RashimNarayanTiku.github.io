db23d7d4bb34f3e5635f0e7132a6a670022a5b15
RashimNarayanTiku <rashim.narayan.tiku@gmail.com>
2021-12-26 23:08:44 +0530

    Cleaned Code, Improved Path and Added Rotation

--- main.js
+++ main.js
@@ -5,32 +5,31 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 
-
+// Scene and Camera
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
-camera.position.set(0,2,500);
+camera.position.set(0,2,1000);
 
-// const renderer = new THREE.WebGL1Renderer({
-//     canvas: document.querySelector('#bg'),
-// });
+// const renderer = new THREE.WebGL1Renderer({ canvas: document.querySelector('#bg') });
 // renderer.setPixelRatio(window.devicePixelRatio);
 // renderer.setSize(window.innerWidth, window.innerHeight);
 
-
+// Renderer
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
+
+// Lights and OrbitControls
 const pointLight = new THREE.PointLight(0xffffff);
-pointLight.position.set(50,100,25);
+pointLight.position.set(50,100,100);
 const ambientLight = new THREE.AmbientLight(0xffffff);
-const lightHelper = new THREE.PointLightHelper(pointLight);
-const gridHelper = new THREE.GridHelper(200,50);
-scene.add(ambientLight,lightHelper);
-const controls = new OrbitControls(camera, renderer.domElement);
+// const lightHelper = new THREE.PointLightHelper(pointLight);
+// let controls = new OrbitControls(camera, renderer.domElement);
+scene.add(pointLight);
 
 
-// Dynamic Window size updation
+// Dynamic Window Size Updation
 window.addEventListener('resize', onWindowResize, false)
 function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight
@@ -66,8 +65,7 @@ function onWindowResize() {
 // }
 
 
-
-// Making hollow sphere of stars
+// Adding Hollow Sphere of Stars
 const v = new THREE.Vector3();
 function randomPointInSphere(radius) {
 
@@ -83,178 +81,137 @@ function randomPointInSphere(radius) {
   return v;
 }
 
+let particles;
 function makeStarSphere() {
   let geometry = new THREE.BufferGeometry();
   let positions = [];
-  let count = 55000;
-  
+  let count = 10000;
+  let radius = 100;
   for (var i = 0; i<count; i++) {
-      var vertex = randomPointInSphere(200);
+      var vertex = randomPointInSphere(radius);
       positions.push( vertex.x, vertex.y, vertex.z );
   }
   
   geometry.setAttribute( 'position', new THREE.Float32BufferAttribute( positions, 3 ) );
   let material = new THREE.PointsMaterial( { color: 0xffffff, size: 0.1 } );
-  let particles = new THREE.Points(geometry, material);
+  particles = new THREE.Points(geometry, material);
   scene.add( particles );
 }
 
 
+// Loading 3-D Models 
+let saturn;
 let loader = new GLTFLoader();
 loader.crossOrigin = true;
 
 loader.load("./saturn/scene.glb", function(data) {
-  data.scene.scale.set(0.7, 0.7, 0.7)
+
   let object = data.scene;
   object.position.set(0,0,0);
+  saturn = object;
   scene.add( object );
 });
 
-// loader.load("./moon/scene.gltf", function(data) {
-//   data.scene.scale.set(0.7, 0.7, 0.7)
-//   let object = data.scene.children[0];
-//   object.position.set(0,-3,30);
-//   scene.add( object );
-// });
 
-// loader.load("./earth-real/scene.gltf", function(data) {
-
-//   data.scene.scale.set(0.2, 0.2, 0.2)
-
-//   let object = data.scene.children[0];
-//   object.position.set(0,0,30);
-//   scene.add( object );
-// });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// ----------------------------------RECOMMENDED WAY TO MAKE SPLINE CURVES
-// const curv = new THREE.CatmullRomCurve3( [
-//   new THREE.Vector3(0,2,500),
-//   new THREE.Vector3(0,2,400),
-//   new THREE.Vector3(0,2,350),
-//   new THREE.Vector3(0,2,300),
-//   new THREE.Vector3(0,2,250),
-//   new THREE.Vector3(0,2,200),
-//   new THREE.Vector3(0,2,150),
-//   new THREE.Vector3(-10,2,100),
-//   new THREE.Vector3(-20,2,100),
-//   new THREE.Vector3(0,2,80),
-//   new THREE.Vector3(20,2,100),
-//   new THREE.Vector3(25,2,120),
-//   new THREE.Vector3(30,2,150),
-//   new THREE.Vector3(20,2,200),
-//   new THREE.Vector3(10,2,250),
-//   new THREE.Vector3(5,2,300),
-//   new THREE.Vector3(0,2,350),
-// ], true );
-
-// const poin = curv.getPoints( 500 );
-// const geometr = new THREE.TubeBufferGeometry(curv, 20, 200, 8, true).setFromPoints( poin );
-// const materia = new THREE.MeshBasicMaterial( { color : 0xff0000, wireframe:true } );
-// const curveObject = new THREE.Mesh( geometr, materia );
-// scene.add(curveObject);
-
-
-
-
-
-
-
-
-
+// Camera Animation Path
 let pathCurve = new THREE.CatmullRomCurve3([
-  new THREE.Vector3(0,2,500),
-  new THREE.Vector3(0,2,400),
-  new THREE.Vector3(0,2,350),
-  new THREE.Vector3(0,2,300),
-  new THREE.Vector3(0,2,250),
-  new THREE.Vector3(0,2,200),
+  // new THREE.Vector3(-20,2,500),
+  // new THREE.Vector3(-20,2,400),
+  // new THREE.Vector3(-20,2,350),
+  // new THREE.Vector3(-20,2,300),
+  // new THREE.Vector3(-20,2,250),
+
+  new THREE.Vector3(50,2,150),
+  new THREE.Vector3(35,2,150),
+  new THREE.Vector3(15,2,150),
+  new THREE.Vector3(5,2,150),
   new THREE.Vector3(0,2,150),
-  new THREE.Vector3(-10,2,100),
+  new THREE.Vector3(-10,2,150),
   new THREE.Vector3(-20,2,100),
-  new THREE.Vector3(0,2,80),
-  new THREE.Vector3(20,2,100),
-  new THREE.Vector3(25,2,120),
-  new THREE.Vector3(30,2,150),
-  new THREE.Vector3(20,2,200),
-  new THREE.Vector3(10,2,250),
-  new THREE.Vector3(5,2,300),
-  new THREE.Vector3(0,2,350),
-])
 
+  new THREE.Vector3(-30,2,50),
+  new THREE.Vector3(-30,2,40),
+  new THREE.Vector3(-30,2,30),
+  new THREE.Vector3(-30,2,20),
+  new THREE.Vector3(-30,2,10),
+  new THREE.Vector3(-30,2,0),
+
+  new THREE.Vector3(-30,2,-21),
+  new THREE.Vector3(-30,2,-21),
+  new THREE.Vector3(-30,2,-21),
+  new THREE.Vector3(-30,2,-21),
+  new THREE.Vector3(-30,2,-21),
+  new THREE.Vector3(-20,2,-21),
+  new THREE.Vector3(-10,2,-21),
+  new THREE.Vector3(30,2,-21),
+
+  new THREE.Vector3(30,2,-11),
+  new THREE.Vector3(30,2,0),
+  new THREE.Vector3(30,2,10),
+  new THREE.Vector3(30,2,21),
+  new THREE.Vector3(30,2,31),
+  new THREE.Vector3(30,2,41),
+  new THREE.Vector3(30,2,51),
+  new THREE.Vector3(30,2,81),
+  new THREE.Vector3(30,2,120),
+
+  
+  new THREE.Vector3(40,2,150),
+  new THREE.Vector3(50,2,150),
+  // new THREE.Vector3(40,2,280),
+  // new THREE.Vector3(30,2,300),
+  // new THREE.Vector3(20,2,350),
+  // new THREE.Vector3(10,2,350),
+
+]);
 let pathGeo = new THREE.TubeGeometry(pathCurve, 20, 2, 8, true);
-let pathMat = new THREE.MeshBasicMaterial({color: 0xffffff, wireframe:true, opacity: 1, transparent: true});
+let pathMat = new THREE.MeshBasicMaterial({ wireframe : true, visible: false });
 let path = new THREE.Mesh(pathGeo, pathMat);
-
-path.geometry.parameters.getpointat
 path.position.z -= 100;
-
-
 scene.add(path);
-// path.geometry.parameters.getpointat()
 
-
-
-
-
-
-
-let cameraTarget = new THREE.Vector3();
-cameraTarget.position = pathMesh.geometry.parameters.getpo
 
 
 //Scroll binding for lerping
-let oldValue = 0
-let newValue = 0
+let oldValue = 0;
+let newValue = 0;
+let progress = 0;
+let lerpAlpha = 0.1;
+
+let cameraTarget = new THREE.Vector3(0,2,500);
+
 window.addEventListener('scroll', (e) => {
+
   newValue = window.pageYOffset;
 
-  //covers both scroll-up and scroll-down cases
-  if(cameraTarget.z <= 25) {
-      if(cameraTarget.z > 5)
-        cameraTarget.z -= (newValue-oldValue) * 0.1;  
-    //   else
-    //     cameraTarget.z += (newValue-oldValue) * 0.05;  
+  progress += (newValue - oldValue) * 0.001;
+  // progress += 0.005;
+  progress = progress % 1; 
+  if(!(progress >= 0 && progress < 1))
+    progress = 0;
+  
+  cameraTarget.copy(pathCurve.getPointAt(progress));
 
-    if(cameraTarget.x < 30)
-      cameraTarget.x += (newValue-oldValue) * 0.3; 
-    else
-      cameraTarget.x -= (newValue-oldValue) * 0.3; 
-
+  if(cameraTarget.x <= 0) {
+    console.log('Adding 15');
+    cameraTarget.x += 15;
+  } else {
+    console.log('Subtracting 15');
+    cameraTarget.x -= 15;
   }
-  else
-    cameraTarget.z -= (newValue-oldValue) * 0.4;  
 
-
-//   oldValue = newValue;
+  renderer.render( scene, camera );
+  // oldValue = newValue;
 });
 
-const data = {
-    lerpAlpha: 0.1,
-    lerpVectorsAlpha: 1.0,
-}
 
 function animate() {
 
-    controls.update();
-    // camera.position.lerp(cameraTarget, data.lerpAlpha);
+    // controls.update();
     camera.lookAt(0,0,0);
-
+    camera.position.lerp(cameraTarget, lerpAlpha);
+    if(saturn) saturn.rotation.y += 0.002;
+    if(particles) particles.rotation.y += 0.0006;
     renderer.render(scene, camera);
     requestAnimationFrame(animate);
 }
