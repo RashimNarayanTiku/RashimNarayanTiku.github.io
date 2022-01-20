a6b215b77a9a7df91a41cce8d126dd0e0d8bf4aa
RashimNarayanTiku <rashim.narayan.tiku@gmail.com>
2022-01-21 00:01:45 +0530

    loader changes

--- main.js
+++ main.js
@@ -307,14 +307,18 @@ function resumeAnimation() {
 function changeLoaderText() {
   setTimeout(() => {
     document.getElementById('loading-text').innerHTML = 'You won\'t be dissapointed...';
-    let margin_left = screen.width > 586 ? '40vw' : '18vw';
+    let margin_left = screen.width > 586 ? '41vw' : '18vw';
     document.getElementById('loading-text').parentNode.style.marginLeft = margin_left;
-  
   }, 20*1000);
 
   setTimeout(() => {
     document.getElementById('loading-text').innerHTML = 'Well that\'s embarrassing...';  
-  }, 45*1000);
+  }, 40*1000);
+
+  setTimeout(() => {
+    document.getElementById('loading-text').innerHTML = 'Who wants ice-cream! 🍦';  
+  },60*1000);
+
 }
 
 $(function() {
