d88663b42c38503652b1e6dac145f5a0aacb195b
RashimNarayanTiku <rashim.narayan.tiku@gmail.com>
2022-10-10 22:31:43 +0530

    Adjust variable for scroll indicator color change

--- main.js
+++ main.js
@@ -187,11 +187,11 @@ function isInViewport(elem) {
   var rect = element.getBoundingClientRect();
   var html = document.documentElement;
   return (
-    rect.top >= 0 &&
+    rect.top >= -25 &&
     rect.left >= 0 &&
     rect.bottom <= (window.innerHeight || html.clientHeight) &&
     rect.right <= (window.innerWidth || html.clientWidth)
-  )
+  );
 }
 
 
