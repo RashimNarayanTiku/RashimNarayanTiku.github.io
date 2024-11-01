cf7fed67693053bcf9f44e5680164ea404e4faf4
TIKU Rashim Narayan <RTIK3OX6@as.airbus.corp>
2024-11-01 19:02:20 +0530

    Move 'cpu speed test' after 'model download' to remove its delay

--- preload.js
+++ preload.js
@@ -1,8 +1,30 @@
 export const isScreenSmall = window.innerWidth <= 576;
 export const canTestSpeed = navigator.connection && navigator.connection.downlink;
-export const slowCPU = CPUSpeedTest();
 export let highBandwidth = false;
 
+// Testing network speed
+if(!canTestSpeed) {
+  console.log('Unable to test network speed, smaller 3D model will be shown by default.');
+} else if(navigator.connection.downlink > 5) {
+  highBandwidth = true;
+} else {
+  console.log('Slow network speed detected, showing smaller 3D model.');
+}
+
+// Caching 3D model
+export let cachedSaturnModel = null;
+async function preloadSaturn(isScreenSmall, highBandwidth) { 
+  const modelName = isScreenSmall || !highBandwidth ? "saturn-compressed" : "saturn"; 
+  const modelURL = `/saturn/${modelName}.glb`; 
+  if (!cachedSaturnModel) { 
+    const response = await fetch(modelURL); 
+    cachedSaturnModel = await response.arrayBuffer(); 
+  } 
+}
+
+preloadSaturn(isScreenSmall, highBandwidth);
+
+// Testing CPU speed
 function CPUSpeedTest() {
   const calculation = () => { 
     let sum = 0; 
@@ -20,23 +42,4 @@ function CPUSpeedTest() {
   }
   return false
 }
-
-if(!canTestSpeed) {
-  console.log('Unable to test network speed, smaller 3D model will be shown by default.');
-} else if(navigator.connection.downlink > 5) {
-  highBandwidth = true;
-} else {
-  console.log('Slow network speed detected, showing smaller 3D model.');
-}
-
-export let cachedSaturnModel = null;
-async function preloadSaturn(isScreenSmall, highBandwidth) { 
-  const modelName = isScreenSmall || !highBandwidth ? "saturn-compressed" : "saturn"; 
-  const modelURL = `/saturn/${modelName}.glb`; 
-  if (!cachedSaturnModel) { 
-    const response = await fetch(modelURL); 
-    cachedSaturnModel = await response.arrayBuffer(); 
-  } 
-}
-
-preloadSaturn(isScreenSmall, highBandwidth);
+export const slowCPU = CPUSpeedTest();
