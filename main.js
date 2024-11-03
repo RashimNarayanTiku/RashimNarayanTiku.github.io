685f47ce52ad98fbb64de2fb8f2795f1863e5970
TIKU Rashim Narayan <RTIK3OX6@as.airbus.corp>
2024-11-03 15:38:11 +0530

    outerHeight to canvas

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import { cachedSaturnModel, isScreenSmall, slowCPU } from "./preload.js";
 // Renderer, Scene, Camera, Light
 const renderer = new THREE.WebGL1Renderer({ canvas: document.querySelector('#bg') });
 renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setSize(window.outerWidth, window.outerHeight);
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.set(0, 2, 1200);
